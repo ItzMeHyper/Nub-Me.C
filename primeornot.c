32494bac0589e1407e7bdcbdf9e7233d5c6c5a6a
Hyper <133499771+ItzMeHyper@users.noreply.github.com>
2024-03-22 12:35:22 +0530

    Update primeornot.c

--- primeornot.c
+++ primeornot.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
-int main() {
-    int n, i;
+void main() {
+    int n, i, isPrime = 1;
     
     printf("Enter a number: ");
     scanf("%d", &n);
     
-    for(i = 2; i < n; i++) {
+    if(n == 0 || n == 1){
+        isPrime = 0;
+    }
+    
+    for(i = 2; i <= n/2; i++) {
         if(n % i == 0) {
-            printf("%d is not prime\n", n);
-            return 0; // Exiting the program after finding a divisor
+            isPrime = 0;
+            break;
         }
     }
     
-    printf("%d is prime\n", n);
-    
-    return 0;
+    if(isPrime == 1) {
+        printf("%d is prime\n", n);
+    } else {
+        printf("%d is not prime\n", n);
+    }
 }
