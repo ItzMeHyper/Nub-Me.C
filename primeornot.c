700d9c9faf17bf9fc041ab4505f9efd1a426689b
Hyper <133499771+ItzMeHyper@users.noreply.github.com>
2024-04-01 18:18:47 +0530

    Update primeornot.c

--- primeornot.c
+++ primeornot.c
@@ -3,23 +3,22 @@
 void main() {
     int n, i, isPrime = 1;
     
-    printf("Enter a number: ");
+    printf("Enter a number: \n");
     scanf("%d", &n);
     
-    if(n == 0 || n == 1){
-        isPrime = 0;
-    }
-    
-    for(i = 2; i <= n/2; i++) {
-        if(n % i == 0) {
-            isPrime = 0;
-            break;
-        }
-    }
-    
-    if(isPrime == 1) {
-        printf("%d is prime\n", n);
+    if(n == 1 || n == 0){
+        printf("%d is neither prime nor composite\n", n);
     } else {
-        printf("%d is not prime\n", n);
+         for(i = 2; i <= n/2; i++) {
+            if(n % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if(isPrime == 1) {
+            printf("%d is prime\n", n);
+        } else {
+            printf("%d is not a prime number\n", n);
+        }
     }
 }
