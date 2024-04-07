1858aaab73ca366d216d0b5c7a3c91ec1f736a5c
Hyper <133499771+ItzMeHyper@users.noreply.github.com>
2024-04-07 18:27:31 +0530

    Add files via upload

---
+++ Array/elemnts_bubble_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void main() {
+    int i, j, n, temp;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n]; 
+
+    printf("Enter the numbers: \n");
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+
+        for(j = 0; j < n-i-1; j++) {
+
+            if(arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printf("The sorted elemts are: \n");
+    for(i = 0; i<n; i++){
+        printf("%d \t", arr[i]);
+    }
+}
