81317094e8edae78f8e9fff5e4cbb90c84f22fe7
Hyper <133499771+ItzMeHyper@users.noreply.github.com>
2024-04-07 17:19:05 +0530

    Add files via upload

---
+++ Array/sumavgofElmtsarray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main() {
+    int n, sum = 0, i;
+    double avg ;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter the numbers: \n");
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+
+        sum = sum + a[i];
+    }
+
+    avg = (double)sum/4 ;
+
+    printf("The sum of the numbers is %d \n", sum);
+    printf("The average of the numbers is %lf \n", avg);
+}
